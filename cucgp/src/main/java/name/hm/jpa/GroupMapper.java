d8bc00d869cfab7f02810675c21e0b1b259d9acd
HouMing <houm1988@qq.com>
2013-02-23 01:36:30 +0800

    add log4j for celltest

--- cucgp/src/main/java/name/hm/jpa/GroupMapper.java
+++ cucgp/src/main/java/name/hm/jpa/GroupMapper.java
@@ -6,22 +6,39 @@ import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Result;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 public interface GroupMapper {
 
-	final String SELECT_BY_ID = "SELECT * FROM cucgp.group WHERE group_id = #{param1}";
+	final String SELECT_BY_GROUPID = "SELECT * FROM cucgp.group WHERE group_id = #{param1}";
+	final String SELECT_BY_GROUPNAME = "SELECT * FROM cucgp.group WHERE group_name = #{param1}";
 	final String INSERT_GROUP = "INSERT INTO cucgp.group (group_id, group_name, valide) VALUES " +
 			                    "(#{groupId}, #{groupName}, #{valide})";
+	final String UPDATE = "UPDATE cucgp.group " +
+			              "SET group_name = #{groupName}, valide = #{valide} " +
+			              "WHERE group_id = #{groupId}";
 	
 	//Basic CRUD method
-	@Select(SELECT_BY_ID)
+	@Select(SELECT_BY_GROUPID)
 	@Results(value = {
 			@Result(property="groupId",column="group_id"),
 			@Result(property="groupName",column="group_name"),
 			@Result(property="valide",column="valide")
 	})
-	Group select(Integer id);
+	Group selectByGroupId(Integer groupId);
 
+	@Select(SELECT_BY_GROUPNAME)
+	@Results(value = {
+			@Result(property="groupId",column="group_id"),
+			@Result(property="groupName",column="group_name"),
+			@Result(property="valide",column="valide")
+	})
+	Group selectByGroupName(String groupName);
+	
 	@Insert(INSERT_GROUP)
 	void insert(Group cellTest);
+
+	@Update(UPDATE)
+	void update(Group grp);
+
 }
--- cucgp/src/test/java/name/hm/test/cell/GroupCellTest.java
+++ cucgp/src/test/java/name/hm/test/cell/GroupCellTest.java
@@ -2,7 +2,9 @@ package name.hm.test.cell;
 
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.log4j.Logger;
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
@@ -15,68 +17,114 @@ import name.hm.test.CellTest;
  * TODO test ISUD of Group Table
  */
 @Category(CellTest.class)
-public class GroupCellTest {
-	SqlSessionFactory factory = EnvTest.getSqlSessionFactory();
+  public class GroupCellTest {
+    SqlSessionFactory factory;
+    SqlSession se = null;
+    GroupMapper mp = null;
+    private static Logger logger = Logger.getLogger("celltest");
+    @Before
+      public void init() {
+    	logger.info("start GroupCellTest");
+        factory = EnvTest.getSqlSessionFactory();
+        se = factory.openSession();
+        mp = se.getMapper(GroupMapper.class);
+      }
 
-	/**
-	 * TODO insert group #groupId(0) #groupName("CellTest") #valide('invalide')
-	 */
-	@Test
-	public void insertGroup() {
-		SqlSession se = factory.openSession();
-		try {
-			Group cellTest = new Group();
-			cellTest.setGroupId(0);
-			cellTest.setGroupName("CellTest");
-			cellTest.setValide("invalide");
-			GroupMapper mapper = se.getMapper(GroupMapper.class);
-			mapper.insert(cellTest);
-			se.commit();
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			se.close();
-		}
-	}
+    @After
+      public void clean() {
+    	logger.info("end GroupCellTest");
+        se.close();
+      }
 
-	/**
-	 * TODO select group #groupId(0) #groupName("CellTest") #valide('valide') =>
-	 * #valide('invadile')
-	 */
-	@Test
-	public void selectGroup() {
-		SqlSession se = factory.openSession();
-		try {
-			GroupMapper mapper = se.getMapper(GroupMapper.class);
-			Group grp = mapper.select(0);
-			se.commit();
-			System.out.println(grp);
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			se.close();
-		}
-	}
+    @Test
+      public void insertGroup() {
+        logger.info("GroupCellTest - insertGroup\n" +
+                           " PASSED 2013/02/22 20:00\n" +
+                           " insert group\n" +
+                           " #groupId(0)\n" +
+                           " #groupName(\"CellTest\")\n" +
+                           " #valide('invalide')\n");
+        try {
+          se.flushStatements();
+          Group cellTest = new Group();
+          cellTest.setGroupId(0);
+          cellTest.setGroupName("CellTest");
+          cellTest.setValide("invalide");
+          mp.insert(cellTest);
+          se.commit();
+        } catch (Exception e) {
+          se.rollback();
+          e.printStackTrace();
+        }
+      }
 
-	/**
-	 * TODO update group #groupName("CellTest" <--> "CellTestChange")
-	 * #valide("invalide" -> "valide")
-	 */
-	@Test
-	public void updateGroup() {
-		SqlSession se = factory.openSession();
-		try {
-			
-		}finally {
-			se.close();
-		}
-	}
+    /**
+     * TODO
+     * select group
+     * PASSED #groupId(0)
+     * #groupName("CellTest")
+     * #valide('valide') => #valide('invadile')
+     */
+    @Test
+      public void selectGroup() {
+        try {
+          se.flushStatements();
+          Group grp = mp.selectByGroupId(0);
+          Group grp2 = mp.selectByGroupName("CellTest");
+          se.commit();
+          logger.info(grp.toString());
+          logger.info(grp2.toString());
+        } catch (Exception e) {
+          se.rollback();
+          e.printStackTrace();
+        }
+      }
 
-	/**
-	 * TODO delete group #groupId(0)
-	 */
-	@Test
-	public void deleteGroup() {
-	}
+    /**
+     * TODO update group 
+     * #groupName("CellTest" <--> "CellTestChange")
+     * #valide('invalide' -> 'valide')
+     */
+    @Test
+      public void updateGroup() {
+        logger.info("S");
+        StringBuilder strb = new StringBuilder();
+        try {
+          se.flushStatements();
+          strb.append("#groupName(\"CellTest\" <--> \"CellTestChange\") \t");
+          Group grp = mp.selectByGroupId(0);
+          strb.append(grp + "\t");
 
-}
+          grp.setGroupName("CellTestChange");
+          mp.update(grp);
+          grp = mp.selectByGroupId(0);
+          se.commit();
+          strb.append(grp + "\t");
+
+          grp.setGroupName("CellTest");
+          mp.update(grp);
+          grp = mp.selectByGroupId(0);
+          se.commit();
+          strb.append(grp + "\t");
+
+          strb.append("#valide('invalide' -> 'valide')\t");
+          grp.setValide("valide");
+          mp.update(grp);
+          grp = mp.selectByGroupId(0);
+          se.commit();
+          strb.append(grp + "\t");
+
+          logger.info(strb.toString());
+        }finally {
+          se.close();
+        }
+      }
+
+    /**
+     * TODO delete group #groupId(0)
+     */
+    @Test
+      public void deleteGroup() {
+      }
+
+  }
